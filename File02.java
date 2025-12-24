30ab768a1bedee0fe388f8e06ee12b510fd42ad7
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-12-24 10:15:25 +0530

    Add files via upload

---
+++ File02.java
@@ -0,0 +1,10 @@
+
+
+public class File02 {
+    public static void main(String[] args) {
+        String s = "C O M P U T E R  S C I E N C E  E N G I N E E R I N G ";
+
+        s = s.concat("H E L L O");
+        System.out.println(s);
+    }
+}
