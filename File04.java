140f0c67a988c6f117acd972fa4fa9ea461073a5
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-12-29 09:28:54 +0530

    Add files via upload

---
+++ File04.java
@@ -0,0 +1,10 @@
+public class File04 {
+    // String Methods
+
+    public static void main(String[] args) {
+        String S1 = new String("j a v a");
+        String S2 = new String("J A V A");
+        System.out.println(S1 == S2);
+        System.out.println(S1.equalsIgnoreCase(S2));
+    } 
+}
