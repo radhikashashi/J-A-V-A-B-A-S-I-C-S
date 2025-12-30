39469dd6d3badc16ca82e3685fbc70cf6aed0673
SHASHANK <151935629+radhikashashi@users.noreply.github.com>
2025-12-30 11:17:48 +0530

    Method Overloaded

---
+++ File09.java
@@ -0,0 +1,20 @@
+public class File09 {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    static double add(double d1, double d2) {
+        return d1 + d2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(10, 20));
+        System.out.println(add(10, 20, 30));
+        System.out.println(add(5.5, 4.5));
+    }
+}
